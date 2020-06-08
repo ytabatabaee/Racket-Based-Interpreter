79877c2719f8c849ff6d3c3225484906cfc892c7
ce441-981-95104866 <yasamin.tabatabaee@gmail.com>
2020-06-08 22:41:20 +0430

    datatypes

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,12 +1,80 @@
 #lang racket
 
+; todo: add the rest
+(define-datatype cexp cexp?
+  (var
+   (id symbol?))
+  (pos-number
+   (num number?))
+  (neg-exp
+   (body-exp cexp?))
+  (paren-exp
+   (body-exp exp?)))
+
+
+(define-datatype bexp bexp?
+  (cexp-exp
+    (exp1 cexp?))
+  (mul-exp
+    (exp1 cexp?)
+    (exp2 bexp?))
+  (div-exp
+    (exp1 cexp?)
+    (exp2 bexp?)))
+
+
+(define-datatype aexp aexp?
+  (bexp-exp
+    (exp1 bexp?))
+  (sum-exp
+    (exp1 bexp?)
+    (exp2 aexp?))
+  (sub-exp
+    (exp1 bexp?)
+    (exp2 aexp?)))
+
+
+(define-datatype exp exp?
+  (aexp-exp
+    (exp1 aexp?))
+  (gt-exp
+    (exp1 aexp?)
+    (exp2 aexp?))
+  (lt-exp
+    (exp1 aexp?)
+    (exp2 aexp?))
+  (eq-exp
+    (exp1 aexp?)
+    (exp2 aexp?))
+  (neq-exp
+    (exp1 aexp?)
+    (exp2 aexp?)))
+
+(define-datatype exp exp?
+  (while-exp
+    (exp1 aexp?))
+  (if-exp
+    (exp1 aexp?)
+    (exp2 aexp?))
+  (assign-exp
+    (exp1 aexp?)
+    (exp2 aexp?))
+  (ret-exp
+    (exp1 aexp?)
+    (exp2 aexp?))
+  (neq-exp
+    (exp1 aexp?)
+    (exp2 aexp?)))
+
+
+  
 
 (define interpret-cmd
   (lambda (cmd env)
     env
     ))
 
-
+; whilecom | ifcome | assign | return
 (define interpret-unitcom
   (lambda (unitcom env)
     (cond
@@ -40,6 +108,18 @@
 
 ; return exp
 ; parser output: (return (exp))
+
+
+
+(define interpret-exp
+  (lambda (exp env)
+    (cond
+      ((eq? (car exp) '>) (interpret-aexp (cadr exp) env))
+      ((eq? (car exp) '<) (interpret-aexp (cadr exp) env))
+      ((eq? (car exp) '==) (interpret-aexp unitcom env))
+      ((eq? (car exp) '!=) (interpret-aexp unitcom env))
+
+
 
 
 
