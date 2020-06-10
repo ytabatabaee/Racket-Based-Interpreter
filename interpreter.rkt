f4691f4e7226510013f8223d0ddc019e713d6479
ce441-981-95104866 <yasamin.tabatabaee@gmail.com>
2020-06-10 13:34:15 +0430

    interpreter structure + program

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,10 +1,10 @@
 #lang racket
 
 (require "environment.rkt")
-
   
 ; command → unitcom | command; unitcom
 ; parser output: ((uc) (uc) ... (uc))
+; returns updated env
 (define interpret-cmd
   (lambda (cmd env)
     (if (null? cmd)
@@ -14,6 +14,7 @@
 
 ; unitcom → whilecom | ifcom | assign | return
 ; whilecom | ifcom | assign | return
+; returns updated env
 (define interpret-unitcom
   (lambda (unitcom env)
     (cond
@@ -25,64 +26,114 @@
 
 ; if exp then command else command endif
 ; parser output: (if (exp) (command) (command))
+; returns updated env
 (define interpret-ifcom
   (lambda (unitcom env)
-    (if (interpret-exp (cadr unitcom) env)
+    (if (value-of-exp (cadr unitcom) env)
         (interpret-cmd (caddr unitcom) env)
         (interpret-cmd (cadddr unitcom) env))))
 
 
 ; while exp do command end
 ; parser output: (while (exp) (command))
+; returns updated env
 (define interpret-whilecom
   (lambda (unitcom env)
-    (if (interpret-exp (cadr unitcom) env)
+    (if (value-of-exp (cadr unitcom) env)
         (interpret-whilecom unitcom (interpret-cmd (caddr unitcom) env))
         env)))
 
 
 ; variable = exp
 ; parser output: (assign variable (exp))
+; returns updated env
 (define interpret-assign
   (lambda (unitcom env)
     (let
         ((var (cadr unitcom))
-         (val (interpret-exp-value (caddr unitcom) env)))
+         (val (value-of-exp (caddr unitcom) env)))
       (update-env var val env))))
       
  
-
-
 ; return exp
 ; parser output: (return (exp))
+; returns value of return exp
 (define interpret-return
-  (lambda (unitcom env)
-    env
-    ; todo complete this
-    ))
-
-
-(define interpret-exp-value
   (lambda (exp env)
-    env
-    ; todo complete this
-    ))
+    (value-of-exp exp env)))
 
 
-(define interpret-aexp
-  (lambda (exp env)
-    env
-    ; todo complete this
-    ))
+
+; todo complete this
+; handle > or < according to doc
+; returns #t or #f
+(define compare
+  (lambda (op)
+    (lambda (exp1 exp2)
+      #t)))
 
 
-(define interpret-exp
+; todo complete this
+; handle == according to doc (as much as i've seen, != is just the opposite of ==)
+; returns #t or #f
+(define is-equal
+    (lambda (exp1 exp2)
+      #t))
+
+
+; todo complete this
+; returns #t or #f
+; op is +, -, *, /
+; we may need to break this function into pieces, idk
+(define binary-operation
+  (lambda (op)
+    (lambda (exp1 exp2)
+    #t)))
+
+
+; todo complete this
+(define negation
+   (lambda (cexp)
+    #t))
+
+
+; parser output: (op cexp bexp) or (cexp)
+(define value-of-cexp
   (lambda (exp env)
     (cond
-      ((eq? (car exp) '>) (interpret-aexp (cadr exp) env))
-      ((eq? (car exp) '<) (interpret-aexp (cadr exp) env))
-      ((eq? (car exp) '==) (interpret-aexp (cadr exp) env))
-      ((eq? (car exp) '!=) (interpret-aexp (cadr exp) env)))))
+      ((eq? (car exp) '-) ((binary-operation *) (value-of-cexp (cadr exp) env) (value-of-bexp (caddr exp) env)))
+      ((eq? (car exp) '() ((binary-operation /) (value-of-cexp (cadr exp) env) (value-of-bexp (caddr exp) env)))
+      (else (value-of-cexp (car exp) env)))))
+
+  
+; parser output: (op cexp bexp) or (cexp)
+(define value-of-bexp
+  (lambda (exp env)
+    (cond
+      ((eq? (car exp) '*) ((binary-operation *) (value-of-cexp (cadr exp) env) (value-of-bexp (caddr exp) env)))
+      ((eq? (car exp) '/) ((binary-operation /) (value-of-cexp (cadr exp) env) (value-of-bexp (caddr exp) env)))
+      (else (value-of-cexp (car exp) env)))))
+
+
+; parser output: (op bexp aexp) or (bexp)
+(define value-of-aexp
+  (lambda (exp env)
+    (cond
+      ((eq? (car exp) '+) ((binary-operation +) (value-of-bexp (cadr exp) env) (value-of-aexp (caddr exp) env)))
+      ((eq? (car exp) '-) ((binary-operation -) (value-of-bexp (cadr exp) env) (value-of-aexp (caddr exp) env)))
+      (else (value-of-bexp (car exp) env)))))
+
+
+; parser output: (op aexp aexp) or (aexp)
+(define value-of-exp
+  (lambda (exp env)
+    (cond
+      ((eq? (car exp) '>) ((compare >) (value-of-aexp (cadr exp) env) (value-of-aexp (caddr exp) env)))
+      ((eq? (car exp) '<) ((compare <) (value-of-aexp (cadr exp) env) (value-of-aexp (caddr exp) env)))
+      ((eq? (car exp) '==) (is-equal (value-of-aexp (cadr exp) env) (value-of-aexp (caddr exp) env)))
+      ((eq? (car exp) '!=) (not (is-equal (value-of-aexp (cadr exp) env) (value-of-aexp (caddr exp) env))))
+      (else (value-of-aexp (car exp) env)))))
+
 
 
 
---
+++ program.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(require "lexer.rkt")
+(require "parser.rkt")
+(require "interpreter.rkt")
+
+; use this file as a glue for all parts
+
+
+; todo complete this
+(define evaluate
+  (lambda (filename)
+    #t))
+  
