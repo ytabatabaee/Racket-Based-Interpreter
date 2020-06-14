0c2430e4032d55c0bf57f17d917b8601f1bf01c7
Rozhiin <nobahari.r@gmail.com>
2020-06-14 14:38:06 +0430

    list functions added, still some problems remained

--- interpreter.rkt
+++ interpreter.rkt
@@ -60,65 +60,149 @@
 ; returns value of return exp
 (define interpret-return
   (lambda (exp env)
-    (value-of-exp exp env)))
+    (value-of-exp (cadr exp) env)))
 
 
-; todo add list compare
 ; handle > or < according to doc
 ; returns #t or #f
 (define compare
 	(lambda (op)
 		(lambda (val1 val2)
 		(cond
-			[(and (string? val1) (string? val2))
-				(if (eqv? op <)
-					(string<? val1 val2)
-					(if (eqv? op >)
-						(string>? val1 val2)))]
+                  [(and (string? val1) (string? val2))
+                   (if (eqv? op <)
+                       (string<? val1 val2)
+                       (if (eqv? op >)
+                           (string>? val1 val2)
+                           (error "Error: Operation not allowed!")
+                           ))]
 
-			[(and (number? val1) (number? val2))
-				(op val1 val2)]
+                  [(and (number? val1) (number? val2))
+                   (op val1 val2)]
+                        
+                  [(list? val1)
+                   (if (compare op (car val1) val2)
+                       (compare op (cdr val1) val2)
+                       #f)]
+                        
+                  [(list? val2)
+                   (if (compare op val1 (car val2))
+                       (compare op val1 (cdr val2))
+                       #f)]
 		))))
 
 
 
-; todo add list is-equal
 ; handle == according to doc (as much as i've seen, != is just the opposite of ==)
 ; returns #t or #f
 (define is-equal
-    (lambda (val1 val2)
-			(equal? val1 val2)
-    ))
+  (lambda (val1 val2)
+    (cond
+      [(and (list? val1) (list? val2))
+       (if (is-equal (value-of-exp (car val1)) (value-of-exp (car val2)))
+           (is-equal (cdr val1) (cdr val2))
+           (#f))]
+    
+      [(list? val1)
+       (if (eqv? (value-of-exp(car val1)) val2)
+           (compare (cdr val1) val2)
+           #f)]
+    
+      [(list? val2)
+       (if (eqv? val1 (value-of-exp (car val2)))
+           (compare val1 (cdr val2))
+           #f)]
+
+      [else (equal? val1 val2)]
+      ))
+  )
+  
 
 
-; todo complete this
-; returns #t or #f
+; returns #t or #f (I don't think so)
 ; op is +, -, *, /
 ; we may need to break this function into pieces, idk
 (define binary-operation
   (lambda (op)
     (lambda (val1 val2)
-			(cond
-				[(and (number? val1) (number? val2))
-					(op val1 val2)]
+      (cond
+        [(and (number? val1) (number? val2))
+         (op val1 val2)]
 
-				[(and (boolean? val1) (boolean? val2))
-					(if (eqv? op +) (or val1 val2)
-						(if (eqv? op *) (and val1 val2)) (error "Error: Operation not allowed!"))]
+        [(and (boolean? val1) (boolean? val2))
+         (if (eqv? op +) (or val1 val2)
+             (if (eqv? op *) (and val1 val2) (error "Error: Operation not allowed!")))]
 
-				[(and (string? val1) (string? val2))
-					(if (eqv? op +) (string-append val1 val2) (error "Error: Operation not allowed!"))]
+        [(and (string? val1) (string? val2))
+         (if (eqv? op +) (string-append val1 val2) (error "Error: Operation not allowed!"))]
 
-			))))
+        [(and (list? val1) (list? val2) (op +))
+         (append val1 val2)]
+
+        [(and (list? val1) (string? val2) (op +))
+         (if (null? val1)
+             '()
+             (cons (string-append val2 (car val1))
+                   (binary-operation op (cdr val1) val2))
+             )]
+
+        [(and (string? val1) (list? val2) (op +))
+         (if (null? val2)
+             '()
+             (cons (string-append val1 (car val2))
+                   (binary-operation op val1 (cdr val2)))
+             )]
+
+        [(and (list? val1) (boolean? val2))
+         (if (null? val1)
+             '()
+             (cons (binary-operation op (car val1) val2)
+                   (binary-operation op (cdr val1) val2))
+             )]
+
+        [(and (list? val2) (boolean? val1))
+         (if (null? val2)
+             '()
+             (cons (binary-operation op (car val2) val1)
+                   (binary-operation op (cdr val2) val1))
+             )]
+
+        [(and (list? val1) (number? val2))
+         (if (null? val1)
+             '()
+             (if (number? (car val1))
+                 (cons (op (car val1) val2)
+                       (binary-operation op (cdr val1) val2))
+                 (error "ERROR: All list members must be of type number."))
+             )]
+
+        [(and (number? val1) (list? val2))
+         (if (null? val2)
+             '()
+             (if (number? (car val2))
+                 (cons (op val1 (car val2)
+                       (binary-operation op val1 (cdr val2))))
+                 (error "ERROR: All list members must be of type number."))
+             )]
+        ))))
 
 
-; todo add list negation
 (define negation
    (lambda (cexp)
-	 	(cond
-			[(number? cexp) (- cexp)]
-			[(boolean? cexp) (not cexp)]
-			)
+     (cond
+       [(number? cexp) (- cexp)]
+       
+       [(boolean? cexp) (not cexp)]
+       
+       [(list? cexp)
+        (if (null? cexp)
+            '()
+            (if (number? (car cexp))
+                (cons (- (car cexp))
+                      (negation (cdr cexp)))
+                (error "ERROR: All list members must be of type number."))
+            )]
+       )
     ))
 
 ; todo complete this
@@ -127,20 +211,26 @@
 (define value-of-cexp
   (lambda (exp env)
     (cond
-      ((eq? (car exp) '-) (negation (value-of-cexp (cadr exp) env)))
-      ((eq? (car exp) 'lparen) (if (eq? (caddr exp) 'rparen)
+      ((eq? (car exp) 'neg) (negation (value-of-cexp (cadr exp) env)))
+      ((eq? (car exp) 'par) (if (eq? (caddr exp) 'rparen)
                                    (value-of-exp (cadr exp) env)
                                    (error "Error: Parenteses not closed!")))
-      ((number? (car exp)) (car exp))
+      ((eq? (car exp) 'num) (cadr exp))
       ((eq? (car exp) 'null) '())
       ((eq? (car exp) 'true) #t)
       ((eq? (car exp) 'false) #f)
-      ((string? (car exp)) (car exp))
+      ((eq? (car exp) 'string) (car exp))
       ; not sure how to handle this, should we have a function for value of listvals?
       ((eq? (car exp) 'lbacket) (if (eq? (caddr exp) 'rbracket)
                                    (value-of-cexp (cadr exp) env)
                                    (error "Error: Bracket not closed!")))
-      ; check listValues
+  
+      ((eq? (car exp) 'list) (if (eq? (caadr exp) 'empty)
+                                 '()
+                                 (display (value-of-exp (list 'list (cdadr exp))))
+                                 ;(cons (value-of-exp (caadr exp))
+                                  ;     (value-of-exp (list 'list (cdadr exp))))
+                                 ))
       ; check listmem
       )))
 
@@ -151,7 +241,7 @@
     (cond
       ((eq? (car exp) '*) ((binary-operation *) (value-of-cexp (cadr exp) env) (value-of-bexp (caddr exp) env)))
       ((eq? (car exp) '/) ((binary-operation /) (value-of-cexp (cadr exp) env) (value-of-bexp (caddr exp) env)))
-      (else (value-of-cexp (car exp) env)))))
+      (else (value-of-cexp exp env)))))
 
 
 ; parser output: (op bexp aexp) or (bexp)
@@ -160,7 +250,7 @@
     (cond
       ((eq? (car exp) '+) ((binary-operation +) (value-of-bexp (cadr exp) env) (value-of-aexp (caddr exp) env)))
       ((eq? (car exp) '-) ((binary-operation -) (value-of-bexp (cadr exp) env) (value-of-aexp (caddr exp) env)))
-      (else (value-of-bexp (car exp) env)))))
+      (else (value-of-bexp exp env)))))
 
 
 ; parser output: (op aexp aexp) or (aexp)
@@ -171,10 +261,15 @@
       ((eq? (car exp) '<) ((compare <) (value-of-aexp (cadr exp) env) (value-of-aexp (caddr exp) env)))
       ((eq? (car exp) '==) (is-equal (value-of-aexp (cadr exp) env) (value-of-aexp (caddr exp) env)))
       ((eq? (car exp) '!=) (not (is-equal (value-of-aexp (cadr exp) env) (value-of-aexp (caddr exp) env))))
-      (else (value-of-aexp (car exp) env)))))
+      (else (value-of-aexp exp env)))))
 
 
 ; todo complete this
 (define id?
   (lambda (sym)
     #t))
+
+
+;(interpret-cmd '((return (false)) (return (true))) '())
+;(interpret-cmd '((while (mult (num 10) (list ((num 34) NULL empty))) ((return (true))))) '())
+;(interpret-cmd '((if (num 10) ((return (true))) ((return (false))))) '())
