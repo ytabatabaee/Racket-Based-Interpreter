abbbf7399fd1d86b9f1863d42a3c32ee5f4f7b7b
y.tabatabaee76 <yasamin.tabatabaee@gmail.com>
2020-07-24 16:50:44 +0430

    initial library function drafts

---
+++ initial-library.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(define pow
+  (lambda (a b)
+    0))
+
+(define make-list
+  (lambda (a b)
+    0))
+
+(define reverse
+  (lambda (a)
+    0))
+
+(define reverse-all
+  (lambda (a)
+    0))
+
+(define set
+  (lambda (a index value)
+    0))
+
+(define merge
+  (lambda (a b)
+    0))
+
+(define merge-sort
+  (lambda (a)
+    0))
+
+(define eval
+  (lambda (s)
+    0))
